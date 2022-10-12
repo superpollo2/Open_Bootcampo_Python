2ba57730b34cd982718afb4b5437be5014f74e36
laura tascon <laura.tascon@udea.edu.co>
2022-10-11 22:59:27 -0500

    ejercicio 9.1

---
+++ ejercicio9.1.py
@@ -0,0 +1,7 @@
+
+country = ['colombia', 'mexico','españa', 'londres', 'panama', 'españa', 'guatemala']
+country.sort()
+country_order = set(country)
+
+for country in country_order:
+    print(country)
